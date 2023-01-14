9b2d0c797602f4cf8105c39c16e13a1560f95b9f
ETHAN <ethanjsifferman@cs.ucsb.edu>
2023-01-14 11:45:38 -0800

    refactored program/ and added `program_stop`

---
+++ programs/aux/program_stop.h
@@ -0,0 +1,11 @@
+/*
+ * File: program_stop.h
+ * Description: Run this function to stop a program.
+ */
+
+#ifndef __PROGRAM_STOP_H
+#define __PROGRAM_STOP_H
+
+extern void program_stop(int exit_code);
+
+#endif
